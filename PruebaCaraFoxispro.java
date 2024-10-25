1fc88ee83f33cdcdead125a112be55a89706e52c
Foxispro <diazospinamanuel@gmail.com>
2024-10-24 21:18:25 -0500

    Imprimir cara de Foxispro

---
+++ PruebaCaraFoxispro.java
@@ -0,0 +1,22 @@
+public class PruebaCaraFoxispro {
+    public static void main(String[] args) {
+
+        System.out.println("                          |\\_____/| ");
+        System.out.println("                         <  o   o  >       /");
+        System.out.println("                         <____  ___>     \\/");
+        System.out.println("                          \\   \\/  /");
+        System.out.println("                           \\_____/");  
+        System.out.println(""); 
+        System.out.println("$$$$$$$$\\                  $$\\                                         ");
+        System.out.println("$$  _____|                 \\__|                                        "); 
+        System.out.println("$$ |    $$$$$$\\  $$\\   $$\\ $$\\   $$$$$$$\\  $$$$$$\\   $$$$$$\\   $$$$$$\\ "); 
+        System.out.println("$$$$$\\ $$  __$$\\ \\$$\\ $$  |$$ | $$  _____|$$  __$$\\ $$  __$$\\ $$  __$$\\ "); 
+        System.out.println("$$  __|$$ /  $$ | \\$$$$  / $$ |\\$$$$$$\\  $$ /  $$ |$$ |  \\__|$$ /  $$ |"); 
+        System.out.println("$$ |   $$ |  $$ | $$  $$<  $$ | \\____$$\\ $$ |  $$ |$$ |      $$ |  $$ |"); 
+        System.out.println("$$ |   \\$$$$$$  |$$  /\\$$\\ $$ |$$$$$$$  |$$$$$$$  |$$ |      \\$$$$$$  |"); 
+        System.out.println("\\__|    \\______/ \\__/  \\__|\\__|\\_______/ $$  ____/ \\__|       \\______/ "); 
+        System.out.println("                                         $$ |                          "); 
+        System.out.println("                                         $$ |                          "); 
+        System.out.println("                                         \\__|                          ");   
+    }
+}
